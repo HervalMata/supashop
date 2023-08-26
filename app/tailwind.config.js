70663fa13f9de69724b00b421ff5cadb8997f29e
HervalMata <hervalmataamparo@gmail.com>
2023-08-26 11:01:48 -0300

    feat:art: api - added and configured prettier and jest

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,17 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
     content: [
         "./screens/**/*.{js,ts,jsx,tsx}",
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
-    ]
+    ],
+    theme: {
+        colors: {
+            "accent-blue": "#4038D6",
+            "accent-pink": "#EAA6F7",
+            "accent-offwhite": "#F2F0E0",
+            "accent-gray": "#808080",
+            gray: colors.neutral,
+        }
+    }
 }
