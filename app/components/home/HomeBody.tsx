a0f58eee00883966b3e79a56ea518223dd88b4fe
HervalMata <hervalmataamparo@gmail.com>
2023-08-31 02:31:39 -0300

    feat:art: app - insert products in home screen

--- app/components/home/HomeBody.tsx
+++ app/components/home/HomeBody.tsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React, { useState } from 'react';
 
-export default function HomeBody({ products }: { products: any }): JSX.Element {
+export default function HomeBody({ products }: { products: any | null }): JSX.Element {
+  const [products, setProducts] = useState<any[] | null>([]);
+
+  useEffect(() => {
+    setProducts(products);
+  }, []);
+  
   return (
     <View className="rounded-t-3x1 px-4 pt-10 -mt-5 bg-gray-50">
-      <Text>HomeBody</Text>
+      <Text>{products[0].title}</Text>
     </View>
   );
 }
